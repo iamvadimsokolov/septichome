26c9f28c2a9662e46f3d92e4129d19400afdb7ad
Vadim Sokolov <iamvadimsokolov@yandex.ru>
2026-08-27 17:37:43 +0300

    инициализация проекта

---
+++ src/content/blog/two-post.md
@@ -0,0 +1,9 @@
+---
+title: 'two-post'
+description: 'two-two-post'
+pubDate: 2026-07-25
+---
+
+# two-post.md
+
+test text
---
+++ src/utils/consts.ts
@@ -0,0 +1,19 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'astro-template';
+export const SITE_DESCRIPTION = 'Welcome to my astro-template!';
+export const SITE_URL = 'https://astro-template.ru/';
+export const SITE_NAME = 'astro-template';
+export const SITE_NAME_RU = 'Астро-Шаблон';
+
+export const navLinks = [
+  { label: "О нас", href: "/about" },
+  { label: "Блог", href: "/blog" },
+  { label: "Контакты", href: "/contacts" }
+]
+
+export const socialLinks = [
+  { label: "Мы в Телеграм", icon: "tg", href: "https://t.me/iamvadimsokolov" },
+  { label: "Мы в Макс", icon: "max", href: "https://max.ru/u/f9LHodD0cOIlnJ4PnKTtbRoPWDD99c4CJ2AQj5tQ9am0S6COIz2Sru3SDw8" },
+]
